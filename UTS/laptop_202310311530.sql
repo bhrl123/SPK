554da66432fb71f5777239de09bb9627aac153f4
bhrl123 <149471442+bhrl123@users.noreply.github.com>
2023-10-31 15:34:09 +0700

    Add files via upload

---
+++ UTS/laptop_202310311530.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.laptop (nama_laptop,ram,memori_internal,processor,layar,harga_rp,baterai_mah) VALUES
+	 ('HP 14S','8 GB','256 GB','Intel Core i5','14 inci',12000000,10000),
+	 ('ASUS VivoBook 15','16 GB','512 GB','AMD Ryzen 7','15,6 inci',18000000,8000),
+	 ('Dell XPS 13','12 GB','1 TB','Intel Core i7','13,3 inci',20000000,12000),
+	 ('ASUS VivoBook 15','4 GB','256 GB','Intel Celeron','15 inci',4000000,6000),
+	 ('Apple MacBook Air','16 GB','512 GB','Apple M1','13 inci',22500000,14000),
+	 ('Lenovo Ideapad 15','8 GB','256 GB','AMD Ryzen 5','15,6 inci',10800000,9000),
+	 ('MSI GS75 Stealth','32 GB','2 TB','Intel Core i9','17,3 inci',30000000,8000),
+	 ('Lenovo ThinkPad 14','8 GB','512 GB','AMD Ryzen 3','14 inci',8400000,7000),
+	 ('Laptop Acer Aspire A515','16 GB','1 TB','Intel Core i7','15,6 inci',15600000,10000),
+	 ('Dell Inspiron 13','8 GB','256 GB','Intel Core i5','13,3 inci',17000000,12000);
